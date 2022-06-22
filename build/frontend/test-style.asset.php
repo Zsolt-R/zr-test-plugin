fd96d66fdddd82842e7392361c8cc59bd61eb93a
Zsolt Revay-Giran <zsolt.revay.g@gmail.com>
2022-06-22 11:07:31 +0200

    Remove test styles

--- build/frontend/test-style.asset.php
+++ build/frontend/test-style.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => 'dfe98d4db21b425cc0f6');
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => '8c5b220bf6f482881a90');
